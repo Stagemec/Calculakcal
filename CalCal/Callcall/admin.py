4f32c88761522e049ff892e94830d86348e313e7
Davi Roberto Santos da Silva <drss.davi@gmail.com>
2025-06-17 15:57:34 -0300

    dia 2

--- CalCal/Callcall/admin.py
+++ CalCal/Callcall/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from Callcall.models import Topic, Entry
 
 # Register your models here.
+admin.site.register(Topic)
+admin.site.register(Entry)
--- CalCal/Callcall/views.py
+++ CalCal/Callcall/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """pagina principal Callcall"""
+    return render(request, 'Callcall/index.html')
