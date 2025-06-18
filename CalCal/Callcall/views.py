59f383f24faf5ddfe00dcc3047bb33af3c06249a
Davi Roberto Santos da Silva <drss.davi@gmail.com>
2025-06-18 17:05:44 -0300

    dia 2

--- CalCal/Callcall/views.py
+++ CalCal/Callcall/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(request):
     """pagina principal Callcall"""
     return render(request, 'Callcall/index.html')
+
+def topics(request):
+    """mostra todos os asssuntos"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'Callcall/topics.html', context)
